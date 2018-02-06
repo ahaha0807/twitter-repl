1b9e263f4917bc79d4d290ac2726f9c6fd6542b1
ahaha0807 <b5331@oic.jp>
2018-02-06 20:01:12 +0900

    Repository/userの再フォーマット

--- repositories/user.js
+++ repositories/user.js
@@ -1,15 +1,15 @@
-'use strict';
+'use strict'
 module.exports = (sequelize, DataTypes) => {
-  var User = sequelize.define('User', {
-    twitterID: DataTypes.STRING,
-    projectID: DataTypes.STRING,
-    isConnect: DataTypes.BOOLEAN
-  }, {
-    classMethods: {
-      associate: function(models) {
-        // associations can be defined here
-      }
-    }
-  });
-  return User;
-};
+    var User = sequelize.define('User', {
+        twitterID: DataTypes.STRING,
+        projectID: DataTypes.STRING,
+        isConnect: DataTypes.BOOLEAN
+    }, {
+        classMethods: {
+            associate: function (models) {
+                // associations can be defined here
+            },
+        }
+    })
+    return User
+}

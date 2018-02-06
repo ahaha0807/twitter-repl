bede1d62b5574f06362799af8bae6f3ce001359a
ahaha0807 <b5331@oic.jp>
2018-02-06 19:37:39 +0900

    とりまTwitterにPOST出来る関数の追加

---
+++ middlewares/accessor.js
@@ -0,0 +1,19 @@
+const Twitter = require('twitter')
+
+module.exports.sendResponse = function (text) {
+    // FIXME: こんなの（key直書きなんて）絶対おかしいよ！
+    let client = new Twitter({
+        consumer_key: 'UaiUm2YDTAqcbmNKyhY88k0J8',
+        consumer_secret: 'pqHGOpSmdZAHjSJZatIjxUNTwDD5Zsaob6hnvvlurIuf5oxg4l',
+        access_token_key: '951724660537810945-Yi4ijHj4x8r31QLkf0k5jOOPgv5YJsK',
+        access_token_secret: '8Lcb5w0yU6RsUdFTU4vrSYcDtuOZEKx0N4BEIrq9ZSdEh',
+    })
+
+    client.post('statuses/update',
+        {status: text},
+        function (error, tweet) {
+            if (!error) {
+                console.log(tweet)
+            }
+        })
+}

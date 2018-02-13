1f3bcdf9cdca5c0f6c2412ba87407ade4dd1bf5d
ahaha0807 <b5331@oic.jp>
2018-02-14 05:51:20 +0900

    dbのPATH指定にミスが有ったのを修正

--- models/User.js
+++ models/User.js
@@ -1,5 +1,7 @@
 const sqlite3 = require('sqlite3')
-const db = new sqlite3.Database("../db/develop.db", sqlite3.OPEN_READWRITE)
+const path = require('path')
+const dbPath = path.resolve(__dirname, '../db/develop.db')
+const db = new sqlite3.Database(dbPath)
 const moment = require('moment')
 
 class User {

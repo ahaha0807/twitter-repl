b05e260e41245bf1d78b9b8d74f3f79c2b0e3e8d
ahaha0807 <b5331@oic.jp>
2018-02-14 05:52:01 +0900

    models/ProjectをORMを使わないように修正

--- models/Project.js
+++ models/Project.js
@@ -1,30 +1,37 @@
-const languageDictionary = require('./LanguageDictonary')
-
-const Sequelize = require('sequelize')
-const sequelize = new Sequelize('users', '', '', {dialect: 'sqlite', storage: './db/develop.db'})
 const moment = require('moment')
+const sqlite3 = require('sqlite3')
 
-let EditorDB = require('../repositories/editor')
+const path = require('path')
+const dbPath = path.resolve(__dirname, '../db/develop.db')
+const db = new sqlite3.Database(dbPath)
+
 const accessor = require('../middlewares/accessor')
+const languageDictionary = require('./LanguageDictonary')
 
 class Project {
     constructor(_projectId, _language) {
         this.projectId = _projectId
         this.language = _language
         this.code = ''
-        this.editordb = EditorDB(sequelize, Sequelize)
     }
 
 
     static createId() {
-        return 'u'
+        return 'xa03g30d91kh9'
     }
 
 
     static find(projectId) {
-        let db = EditorDB(sequelize, Sequelize)
-        return db.find({
-            where: {projectID: projectId}, order: [['updatedAt', 'DESC']]
+        return new Promise((resolve, reject) => {
+            db.serialize(() => {
+                db.get("SELECT * FROM Editors WHERE projectID = ? ORDER BY createdAt DESC", [projectId], (err, row) => {
+                    if (err) {
+                        reject(err)
+                    } else {
+                        resolve(row)
+                    }
+                })
+            })
         })
     }
 
@@ -33,6 +40,32 @@ class Project {
         return languageDictionary.find(language => {
             return language === selectedLanguage
         }) !== undefined
+    }
+
+
+    static codeRegisterToDB(params) {
+        return new Promise((resolve, reject) => {
+            db.serialize(() => {
+                db.run("INSERT INTO Editors(projectID, code, lineIndex, language, createdAt, updatedAt) " +
+                    "VALUES($pid, $code, $li, $lang, $ca, $ua)",
+                    {
+                        $pid: params.projectId,
+                        $code: params.code,
+                        $li: params.lineIndex,
+                        $lang: params.language,
+                        $ca: moment().format("YYYY-MM-DD HH:mm:ss ZZ"),
+                        $ua: moment().format("YYYY-MM-DD HH:mm:ss ZZ")
+                    },
+                    (err) => {
+                        if (err) {
+                            reject(err)
+                        } else {
+                            resolve()
+                        }
+                    }
+                )
+            })
+        })
     }
 
 
@@ -53,43 +86,31 @@ ID: ${this.projectId}
 
 
     codeAdd(additionalResponse) {
-        // FIXME: ここはこれじゃない。変なレコードが登録されるぞ
-        this.editordb.findOrCreate({
-                where: {projectID: this.projectId}, order: [['updatedAt', 'DESC']]
-            }
-        ).then(data => {
-            let lineIndex = data[0].lineIndex || 0
-
-            this.editordb.create({
-                projectID: this.projectId,
-                language: this.language,
-                code: this.code,
-                lineIndex: lineIndex + 1
+        Project.find(this.projectId)
+            .then(data => {
+                if (data === undefined) {
+                    return 0
+                } else {
+                    return data.lineIndex + 1
+                }
             })
-
-            let responseText = this.getInformation()
-            responseText += 'Page: ' + (lineIndex + 1) + `
+            .then(lineIndex => {
+                Project.codeRegisterToDB({
+                    projectId: this.projectId,
+                    language: this.language,
+                    code: this.code,
+                    lineIndex: lineIndex
+                }).then(() => {
+                    let responseText = this.getInformation()
+                    responseText += 'Page: ' + (lineIndex) + `
 `
-            responseText += additionalResponse
+                    responseText += additionalResponse
 
-            accessor.sendResponse(responseText)
-        }).catch(() => {
-            // FIXME: This is AntiPattern
-            this.editordb.create({
-                projectID: this.projectId,
-                language: this.language,
-                code: this.code,
-                lineIndex: 0
-            }).then(() => {
-                let responseText = this.getInformation()
-                responseText += `Page: 0
-`
-                responseText += additionalResponse
-
-                accessor.sendResponse(responseText)
+                    accessor.sendResponse(responseText)
+                }).catch(err => {
+                    return err
+                })
             })
-        })
-
     }
 }
 

8d2194435525c5f5ce2628dee64ba150f3911b0a
ahaha0807 <b5331@oic.jp>
2018-02-14 18:04:33 +0900

    file DBのマイグレーションの修正

--- migrations/20180113151206-create-file.js
+++ migrations/20180113151206-create-file.js
@@ -18,7 +18,7 @@ module.exports = {
         type: Sequelize.STRING
       },
       code: {
-        type: Sequelize.STRING
+        type: Sequelize.TEXT("long")
       },
       createdAt: {
         allowNull: false,

390f7d603a33c2e90b3e592dba015b4fb06dd258
ahaha0807 <b5331@oic.jp>
2018-02-14 18:51:10 +0900

    マイグレーションファイルの修正

--- migrations/20180113070248-create-editor.js
+++ migrations/20180113070248-create-editor.js
@@ -1,33 +1,36 @@
-'use strict';
+'use strict'
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Editors', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      projectID: {
-        type: Sequelize.STRING
-      },
-      code: {
-        type: Sequelize.STRING
-      },
-      lineIndex: {
-        type: Sequelize.INTEGER
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Editors');
-  }
-};
+    up: (queryInterface, Sequelize) => {
+        return queryInterface.createTable('Editors', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            projectID: {
+                type: Sequelize.STRING
+            },
+            language: {
+                type: Sequelize.STRING
+            },
+            code: {
+                type: Sequelize.STRING
+            },
+            lineIndex: {
+                type: Sequelize.INTEGER
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            }
+        })
+    },
+    down: (queryInterface, Sequelize) => {
+        return queryInterface.dropTable('Editors')
+    }
+}
